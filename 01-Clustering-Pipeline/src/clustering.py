a38c127a6e8bd57a78a39f60f86e38d6814ca242
pierinamv <millav.pm@pucp.edu.pe>
2026-01-03 15:17:16 -0500

    update clustering pipeline

--- 01-Clustering-Pipeline/src/clustering.py
+++ 01-Clustering-Pipeline/src/clustering.py
@@ -1,51 +1,49 @@
 import numpy as np
 
-def kmeans(matriz,n_cluster,n_init=15,max_iter=300,tol=0.0001):
+def kmeans(matrix,n_cluster,n_init=25,max_iter=300,tol=1e-12):
     k = n_cluster
-    obs = matriz.shape[0]
-    vars = matriz.shape[1]
+    obs = matrix.shape[0]
+    vars = matrix.shape[1]
 
-    #t = 0.0001
-    tol = np.full(k,tol)
-
+    #tol = np.full(k,tol)
     SelInertia = np.full(n_init,np.nan)
     SelLabels = np.full((n_init,obs),np.nan)
+    SelCentroids = np.full((n_init,k,vars),np.nan)
 
     for j in range(n_init):
 
         delta = np.full(k,1)
-        Ik_t0 = np.full(k,10000)
-
         #Inicializo centroides de forma aleatoria
-        Centroids = matriz[np.random.choice(obs,k,replace=False)]# k indices de los centroides
-
+        Centroids = matrix[np.random.choice(obs,k,replace=False)]# k indices de los centroides
+                
         it = 0
         while np.any(delta > tol) and it<max_iter:
-              
-            dist2 =np.sum((matriz[:,None,:]-Centroids[None,:,:])**2,axis=2) #shape(obs,k,vars)
-            LABELS = np.argmin(dist2,axis=1) #shape(obs)
-            #print(LABELS)
 
+            prev_Centroids = Centroids.copy()      
+            dist2 =np.sum((matrix[:,None,:]-Centroids[None,:,:])**2,axis=2) #shape(obs,k,vars)
+            LABELS = np.argmin(dist2,axis=1)        
             #Redefinir centroides
-            Ik = np.arange(k)
             for ki in range(k):
                 mask = LABELS == ki
                 if np.any(mask):
-                    Centroids[ki]=matriz[mask].mean(axis=0)
+                    Centroids[ki]=matrix[mask].mean(axis=0)
                 else:
-                    pass
-               
-            Ik = np.array([np.sum(dist2[LABELS == ki, ki]) for ki in range(k)])
-        
-            delta = np.abs(Ik_t0 - Ik)
-            Ik_t0 = Ik
+                    Centroids[ki]=matrix[np.random.choice(obs)]          
+                    
+            delta=np.linalg.norm(Centroids - prev_Centroids, axis=1)
     
             it=it+1
-    
+        dist2 =np.sum((matrix[:,None,:]-Centroids[None,:,:])**2,axis=2) #shape(obs,k,vars)
+        LABELS = np.argmin(dist2,axis=1)
+        Ik = np.array([np.sum(dist2[LABELS == ki, ki]) for ki in range(k)])
+
         SelInertia[j]=np.sum(Ik)
         SelLabels[j]=LABELS
+        SelCentroids[j]=Centroids
 
-    return SelLabels[np.where(SelInertia==np.min(SelInertia))[0][0]]
+    inBest = np.argmin(SelInertia)
+            
+    return SelLabels[inBest],SelInertia[inBest],SelCentroids[inBest]        
 
 #Ejemplo:
-#labels = MyKmeans(matriz,9,n_init=10) #sugerido n_ini>15
+#labels,inertia,centroids = kmeans(matriz,9,n_init=10) #sugerido n_ini>15
