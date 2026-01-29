9d5d799326df1b83a08f9792e3e232e1b902ba08
pierinamv <millav.pm@pucp.edu.pe>
2026-01-28 19:36:20 -0500

    Add max sigma calculation

--- 02-Scientific-Simulation-Data-Engineering/analysis/fig_DeltavsVelocity.py
+++ 02-Scientific-Simulation-Data-Engineering/analysis/fig_DeltavsVelocity.py
@@ -1,27 +1,41 @@
-import numpy as np;import pandas as pd; import matplotlib.pyplot as plt
 from pathlib import Path
+import pandas as pd
+import matplotlib.pyplot as plt
 
 base_dir=Path(__file__).resolve().parent.parent
-file=Path(base_dir/'data_experiments'/'vel_vs_maxSigma.dat')
+dataexp_dir = Path(base_dir/'data_experiments')
 
-data = np.loadtxt(file)
-cols = ['v','max_sigma','delta']
-#v = float(input()) #0.010,0.64,2.25
-df = pd.DataFrame(data=data,columns=cols)
-df1 = df.loc[:,['v','max_sigma','delta']]
-df1['delta'] = df1['delta'].round(2)
-#df1.head()
-dict1={'delta':[],'v_c':[]}
-i=0
+data_dir=Path(dataexp_dir/'concentr_sist12acop.dat')
+cols = ['k','sigma','delta','v','da',
+        'db','vsr','u','t_trans','itmax',
+        'nmax','dt','dx']
+df = pd.read_csv(data_dir,
+            sep=r'\s+',
+            header=None,
+            comment='#',
+            names=cols
+            )
+
+df=df[['k','sigma','delta','v']]
+df['delta'] = df['delta'].round(2)
+df1=df.groupby(['delta','v']).max().reset_index()
+df1.rename(columns={'k':'last_tested_k','sigma':'max_sigma'},inplace=True)
+df1=df1[['delta','v','max_sigma','last_tested_k']]
+df1.to_csv(dataexp_dir/'max_sigma.csv',index=False)
+
+dictVc={'delta':[],'v_c':[]}
 for delta, group in df1.groupby('delta'):
-    g1=group[group['max_sigma']<1e-6]
-    g1_maxv=g1.loc[[g1['v'].idxmax()]]
-    dict1['delta'].append(g1_maxv['delta'])
-    dict1['v_c'].append(g1_maxv['v'])
-    plt.scatter(g1_maxv['delta'],g1_maxv['v'],lw=2.,color='black',s=10)
-    plt.text(g1_maxv['delta'].iloc[0]-0.005,g1_maxv['v'].iloc[0]+2.5,f"{g1_maxv['v'].iloc[0]:.0f}",fontsize=9)
+    g1=group[(group['max_sigma']>0.) & (group['max_sigma']<1e-6)]
+    try:
+        id_vmax=g1['v'].idxmax()
+        g1_maxv=g1.loc[id_vmax,:]
+        dictVc['delta'].append(g1_maxv['delta'])
+        dictVc['v_c'].append(g1_maxv['v'])
+    except:
+        print(f'Critical velocity for delta={delta:.2f} has not be found')
 
-plt.plot(dict1['delta'],dict1['v_c'],lw=0.5,ls='--',color='black')
+dfVc=pd.DataFrame(dictVc)
+plt.plot(dictVc['delta'],dictVc['v_c'],lw=0.5,ls='--',color='black')
 plt.xlim(0.05,0.4);plt.ylim(75,200)
 plt.ylabel(r'$v_c$');plt.xlabel(r'$\delta$')
 plt.title(r'Critical velocity for linear stability ($\sigma_{\max}<10^{-6}$)')
